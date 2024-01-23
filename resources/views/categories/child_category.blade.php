fc9ef057cbb9f288a47f307aceb6418ef2f25f17
mondheridriss <153556326+mondheridriss@users.noreply.github.com>
2024-01-23 23:38:24 +0100

    relationship

--- resources/views/categories/child_category.blade.php
+++ resources/views/categories/child_category.blade.php
@@ -1,12 +1,12 @@
 @php
     $value = null;
-    for ($i=1; $i < $child_category->level; $i++){
+    for ($i=0; $i < $child_category->level; $i++){
         $value .= '--';
     }
 @endphp
-<option @if(old('parent_id') == $child_category->id) selected @endif  value="{{ $child_category->id }}">{{ $value." ".$child_category->getTranslation('name') }}</option>
+<option @if(old('parent_id') == $child_category->id) selected @endif  value="{{ $child_category->id }}">{{ $value."  ".$child_category->getTranslation('name') }}</option>
 @if ($child_category->categories)
-    @foreach ($child_category->categories as $childCategory)
+    @foreach ($child_category->childrenCategories as $childCategory)
         @include('categories.child_category', ['child_category' => $childCategory])
     @endforeach
 @endif
