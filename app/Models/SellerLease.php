0a9e62582c9a14b4f2a627a3697477719670394b
ste_codily <git@codily.net>
2024-03-22 11:44:20 +0100

    seller lease

--- app/Models/SellerLease.php
+++ app/Models/SellerLease.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
+use App\Models\SellerPackage;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class SellerLease extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id','package_id','total','discount','start_date','end_date','roles'];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function package(){
+        return $this->belongsTo(SellerPackage::class ,'package_id');
+    }
+
 }
--- database/migrations/2024_03_20_193146_create_seller_leases_table.php
+++ database/migrations/2024_03_20_193146_create_seller_leases_table.php
@@ -15,7 +15,17 @@ return new class extends Migration
     {
         Schema::create('seller_leases', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('vendor_id') ;
+            $table->integer('package_id') ;
+            $table->integer('total')->nullable() ;
+            $table->integer('discount')->nullable() ;
+            $table->date('start_date')->nullable() ;
+            $table->date('end_date')->nullable() ;
+            $table->text('roles')->nullable() ;
             $table->timestamps();
+
+            $table->foreign('vendor_id')->references('id')->on('users')->onDelete('cascade') ;
+            $table->foreign('package_id')->references('id')->on('seller_packages')->onDelete('cascade') ;
         });
     }
 
