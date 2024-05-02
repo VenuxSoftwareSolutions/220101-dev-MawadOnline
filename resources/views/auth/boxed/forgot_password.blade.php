cc1f8326f6a22cc454bc41c994042256d02464a8
medbadisguesmi <115879005+medbadisguesmi@users.noreply.github.com>
2024-05-02 16:12:41 +0200

    fix reset password'

--- resources/views/auth/boxed/forgot_password.blade.php
+++ resources/views/auth/boxed/forgot_password.blade.php
@@ -40,39 +40,17 @@
                                     <form class="form-default" id="reset_form" role="form" action="{{ route('password.email') }}" method="POST">
                                         @csrf
                                         
-                                        <!-- Email or Phone -->
-                                        @if (addon_is_activated('otp_system'))
-                                            <div class="form-group phone-form-group mb-1">
-                                                <label for="phone" class="fs-12 fw-700 text-soft-dark">{{  translate('Phone') }}</label>
-                                                <input type="tel" id="phone-code" class="form-control{{ $errors->has('phone') ? ' is-invalid' : '' }} rounded-0" value="{{ old('phone') }}" placeholder="" name="phone" autocomplete="off">
-                                            </div>
-
-                                            <input type="hidden" name="country_code" value="">
-                                            
-                                            <div class="form-group email-form-group mb-1 d-none">
-                                                <label for="email" class="fs-12 fw-700 text-soft-dark">{{  translate('Email') }}</label>
-                                                <input type="email" class="form-control rounded-0 {{ $errors->has('email') ? ' is-invalid' : '' }}" value="{{ old('email') }}" placeholder="{{  translate('johndoe@example.com') }}" name="email" id="email" autocomplete="off">
-                                                @if ($errors->has('email'))
-                                                    <span class="invalid-feedback" role="alert">
-                                                        <strong>{{ $errors->first('email') }}</strong>
-                                                    </span>
-                                                @endif
-                                            </div>
-                                            
-                                            <div class="form-group text-right">
-                                                <button class="btn btn-link p-0 text-primary" type="button" onclick="toggleEmailPhone(this)"><i>*{{ translate('Use Email Instead') }}</i></button>
-                                            </div>
-                                        @else
+                                       
                                             <div class="form-group">
                                                 <label for="email" class="fs-12 fw-700 text-soft-dark">{{  translate('Email') }}</label>
                                                 <input type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }} rounded-0" value="{{ old('email') }}" placeholder="{{  translate('johndoe@example.com') }}" name="email" id="email" autocomplete="off">
-                                                @if ($errors->has('email'))
-                                                    <span class="invalid-feedback" role="alert">
-                                                        <strong>{{ $errors->first('email') }}</strong>
+                                                @if(Session::has('error'))
+                                                    <span class="invalid-feedback" style="display: block; " role="alert">
+                                                        <strong> {{ Session::get('error') }}</strong>
                                                     </span>
                                                 @endif
                                             </div>
-                                        @endif
+                                        
 
                                         <!-- Submit Button -->
                                         <div class="mb-4 mt-4">
