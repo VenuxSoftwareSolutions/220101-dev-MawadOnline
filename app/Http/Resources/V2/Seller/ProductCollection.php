1f08bab67e8ca24a15e1195634ce3d8fbdf1a988
unknown <badis.guesmi@hypergroup.com.tn>
2024-11-11 13:58:22 +0100

    fix seller dashboard api

--- app/Http/Resources/V2/Seller/ProductCollection.php
+++ app/Http/Resources/V2/Seller/ProductCollection.php
@@ -28,12 +28,14 @@ class ProductCollection extends ResourceCollection
                     'name' => $data->name,
                     //'thumbnail_img' => uploaded_asset($data->thumbnail_img),
                     'thumbnail_img' => UploadProducts::where('id_product', $data->id)
-                    ->where('type', 'thumbnails')
-                    ->value('path') != null ? static_asset(UploadProducts::where('id_product', $data->id)
-                    ->where('type', 'thumbnails')
-                    ->value('path')) : static_asset('assets/img/placeholder.jpg'),
+                        ->where('type', 'thumbnails')
+                        ->value('path') != null ? static_asset(UploadProducts::where('id_product', $data->id)
+                            ->where('type', 'thumbnails')
+                            ->value('path')) : static_asset('assets/img/placeholder.jpg'),
                     //'price' => format_price($data->unit_price),                    
-                    'price' => $data->getPricingConfiguration() != null ? format_price($data->getPricingConfiguration()->first()->unit_price) : "AED0.00" ,
+                    'price' => ($data->getPricingConfiguration() && $data->getPricingConfiguration()->first() != null)
+                        ? format_price($data->getPricingConfiguration()->first()->unit_price)
+                        : "AED0.00",
                     'current_stock' => $qty,
                     'status' => $data->published == 0 ? false : true,
                     'category' => $data->main_category ? $data->main_category->getTranslation('name') : "",
