09f2e1a3b7b893f7e25c10a3acc26018ef302f3e
Atef Ben Ali <atef.bettaib@gmail.com>
2024-11-15 20:27:46 +0100

    style: format

--- app/Models/ProductAttributeValues.php
+++ app/Models/ProductAttributeValues.php
@@ -2,32 +2,38 @@
 
 namespace App\Models;
 
+use App\Traits\EnhancedRevisionableTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Traits\EnhancedRevisionableTrait;
 
 class ProductAttributeValues extends Model
 {
-    use HasFactory, EnhancedRevisionableTrait;
+    use EnhancedRevisionableTrait, HasFactory;
 
     protected function getLastActionNumber()
     {
         $lastRevision = $this->revisionHistory()->latest('id')->first();
+
         return $lastRevision ? $lastRevision->action_number + 1 : 0;
     }
-    public function attribute() {
-        return $this->belongsTo(Attribute::class,'id_attribute') ;
+
+    public function attribute()
+    {
+        return $this->belongsTo(Attribute::class, 'id_attribute');
     }
 
-    public function unity() {
-        return $this->belongsTo(Unity::class,'id_units') ;
+    public function unity()
+    {
+        return $this->belongsTo(Unity::class, 'id_units');
     }
 
-    public function color() {
-        return $this->belongsTo(Color::class,'id_colors') ;
-    }
-    public function attributeValues() {
-        return $this->belongsTo(AttributeValue::class,'id_values') ;
+    public function color()
+    {
+        return $this->belongsTo(Color::class, 'id_colors');
     }
 
+    public function attributeValues()
+    {
+        return $this->belongsTo(AttributeValue::class, 'id_values');
+    }
 }
