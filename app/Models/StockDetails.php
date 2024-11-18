5d837812d730a97b90102eee10183b6e9d8e0bd2
Atef Ben Ali <atef.bettaib@gmail.com>
2024-11-18 16:57:41 +0100

    style: format

--- app/Models/StockDetails.php
+++ app/Models/StockDetails.php
@@ -2,30 +2,31 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class StockDetails extends Model
 {
     const OPERATION_TYPE_ADDITION = 'Stock addition';
+
     const OPERATION_TYPE_REMOVAL = 'Stock removal';
 
     protected $fillable = [
         'operation_type', 'variant_id', 'warehouse_id',
-        'before_quantity', 'transaction_quantity', 'after_quantity', 'user_comment','seller_id'
+        'before_quantity', 'transaction_quantity', 'after_quantity', 'user_comment', 'seller_id',
     ];
 
     public function warehouse()
     {
         return $this->belongsTo(Warehouse::class);
     }
-        public function productVariant()
+
+    public function productVariant()
     {
         return $this->belongsTo(Product::class, 'variant_id')->withTrashed();
     }
 
-        public function seller() {
-            return $this->belongsTo(User::class, 'seller_id');
-
-        }
+    public function seller()
+    {
+        return $this->belongsTo(User::class, 'seller_id');
+    }
 }
