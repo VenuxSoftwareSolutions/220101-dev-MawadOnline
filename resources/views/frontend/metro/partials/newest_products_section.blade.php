eba788d551b80fb234b1c5a9eceb9763fa2bd00b
chouayeb landoulsi <chouayeb.landoulsi@gmail.com>
2024-11-20 14:28:59 +0100

    Home page new design 3

--- resources/views/frontend/metro/partials/newest_products_section.blade.php
+++ resources/views/frontend/metro/partials/newest_products_section.blade.php
@@ -5,7 +5,7 @@
             <div class="d-flex mb-2 mb-md-3 align-items-baseline justify-content-between">
                 <!-- Title -->
                 <h3 class="fs-16 fs-md-20 fw-700 mb-2 mb-sm-0">
-                    <span class="">{{ translate('New Products') }}</span>
+                    <span class="">{{ translate('Recently Added') }}</span>
                 </h3>
                 <!-- Links -->
                 <div class="d-flex">
