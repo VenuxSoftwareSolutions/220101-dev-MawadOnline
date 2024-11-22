7625ec808a9a9c7c8668d83891f5e4fe9b486980
Atef Ben Ali <atef.bettaib@gmail.com>
2024-11-22 16:38:02 +0100

    style: format

--- app/Models/City.php
+++ app/Models/City.php
@@ -2,19 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use App;
+use Illuminate\Database\Eloquent\Model;
 
 class City extends Model
 {
-    public function getTranslation($field = '', $lang = false){
+    public function getTranslation($field = '', $lang = false)
+    {
         $lang = $lang == false ? App::getLocale() : $lang;
         $city_translation = $this->hasMany(CityTranslation::class)->where('lang', $lang)->first();
+
         return $city_translation != null ? $city_translation->$field : $this->$field;
     }
 
-    public function city_translations(){
-       return $this->hasMany(CityTranslation::class);
+    public function city_translations()
+    {
+        return $this->hasMany(CityTranslation::class);
     }
 
     public function country()
