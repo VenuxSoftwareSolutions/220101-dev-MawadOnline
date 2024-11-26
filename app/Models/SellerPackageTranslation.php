44adcd315bc36eafc96340c98796ce6c50b2a590
Selmi Houssem <s.houssem@devnet.me>
2024-11-26 16:30:20 +0100

    feat: add `casts` attribute to `Seller Package translation` model

--- app/Models/SellerPackageTranslation.php
+++ app/Models/SellerPackageTranslation.php
@@ -6,7 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class SellerPackageTranslation extends Model
 {
-    protected $fillable = ['name', 'lang', 'seller_package_id'];
+    protected $fillable = ['details','name', 'lang', 'seller_package_id'];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'details' => 'array',
+    ];
 
     public function seller_package(){
       return $this->belongsTo(SellerPackage::class);
