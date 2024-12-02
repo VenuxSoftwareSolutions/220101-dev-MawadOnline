5bc197611967993d962e8145f284030cb2110816
Atef Ben Ali <atef.bettaib@gmail.com>
2024-12-02 16:59:05 +0100

    chore: clean-up

--- resources/views/seller/coming_soon.blade.php
+++ resources/views/seller/coming_soon.blade.php
@@ -4,7 +4,8 @@
     <div class="coming-soon-container">
         <img src="../public/assets/img/Coming_Soon_Illustration1.svg" alt="Coming Soon Illustration">
         <h1 style="font-weight: 600;font-size: 32px;line-height: 40px;" class="mt-5">Coming Soon...</h1>
-        <p style="font-weight: 400;font-size: 16px;line-height: 24px;">Exciting updates are on the horizon! We're gearing up to unveil powerful new features designed to supercharge
+        <p style="font-weight: 400;font-size: 16px;line-height: 24px;">Exciting updates are on the horizon! We're gearing up
+            to unveil powerful new features designed to supercharge
             your selling experience. Stay tuned for a smoother, more intuitive dashboard.</p>
     </div>
 @endsection
@@ -12,44 +13,45 @@
 @section('script')
     <script>
         document.addEventListener("DOMContentLoaded", function() {
-        document.getElementById('startTourButton').addEventListener('click', function(event) {
-        event.preventDefault(); // Prevent the default anchor click behavior
-        localStorage.setItem('guide_tour', '0'); // Set local storage as required
-        window.location.href = '{{ route("seller.dashboard") }}'; // Redirect to the dashboard
-    });
-    if (localStorage.getItem('guide_tour') != '0') {
-        if ({{Auth::user()->tour}} == true | {{Auth::user()->id}} != {{Auth::user()->owner_id}} ) {
-            return;
-        }
-    }
+            document.getElementById('startTourButton').addEventListener('click', function(event) {
+                event.preventDefault(); // Prevent the default anchor click behavior
+                localStorage.setItem('guide_tour', '0'); // Set local storage as required
+                window.location.href = '{{ route('seller.dashboard') }}'; // Redirect to the dashboard
+            });
+
+            if (localStorage.getItem('guide_tour') != '0') {
+                if ({{ Auth::user()->tour }} == true | {{ Auth::user()->id }} != {{ Auth::user()->owner_id }}) {
+                    return;
+                }
+            }
             var stepValue = {{ $step }};
 
             var tour_steps = [
-            @foreach($tour_steps as $key => $step)
-            {
-                element: document.querySelector('#{{$step->element_id}}'),
-                title: '{{$step->getTranslation('title')}}',
-                intro: "{{$step->getTranslation('description')}}",
-                position: '{{ $step->getTranslation('lang') === 'en' ? 'right' : 'left' }}'
-            },
-            @endforeach
-        ];
-        var lang = '{{$tour_steps[0]->getTranslation('lang')}}';
-        let tour = introJs();
-        let step_number = 0 ;
-        tour.setOptions({
-            steps: tour_steps ,
-            nextLabel: lang == 'en' ? 'Next' : 'التالي',
-            prevLabel: lang == 'en' ? 'Back' : 'رجوع',
-            exitOnEsc : false ,
-            exitOnOverlayClick : false ,
-            disableInteraction : true ,
-            overlayOpacity : 0.4 ,
-            showStepNumbers : true ,
-            hidePrev : true ,
-            showProgress :true ,
-        });
+                @foreach ($tour_steps as $key => $step)
+                    {
+                        element: document.querySelector('#{{ $step->element_id }}'),
+                        title: '{{ $step->getTranslation('title') }}',
+                        intro: "{{ $step->getTranslation('description') }}",
+                        position: '{{ $step->getTranslation('lang') === 'en' ? 'right' : 'left' }}'
+                    },
+                @endforeach
+            ];
 
+            var lang = '{{ $tour_steps[0]->getTranslation('lang') }}';
+            let tour = introJs();
+            let step_number = 0;
+            tour.setOptions({
+                steps: tour_steps,
+                nextLabel: lang == 'en' ? 'Next' : 'التالي',
+                prevLabel: lang == 'en' ? 'Back' : 'رجوع',
+                exitOnEsc: false,
+                exitOnOverlayClick: false,
+                disableInteraction: true,
+                overlayOpacity: 0.4,
+                showStepNumbers: true,
+                hidePrev: true,
+                showProgress: true,
+            });
 
             tour.onexit(function() {
                 $.ajax({
@@ -59,8 +61,6 @@
                         _token: '{{ csrf_token() }}'
                     }, // Include CSRF token for Laravel
                     success: function(response) {
-                        // Handle success
-
 
                         console.log('User tour status updated successfully');
                     },
@@ -77,9 +77,8 @@
             });
 
             tour.onbeforechange(function(targetElement) {
-
                 if (this._direction === 'backward') {
-                if (stepValue == 3) {
+                    if (stepValue == 3) {
                         window.location.href = '{{ route('catalog.search_page') }}';
                     } else if (stepValue == 12) {
                         window.location.href = '{{ route('seller.lease.index') }}';
@@ -100,8 +99,6 @@
                     }
                     sleep(60000);
                 }
-
-                //tour.exit();
             });
 
             tour.start();
@@ -112,7 +109,6 @@
             } else {
                 tour.goToStepNumber(8);
             }
-
         });
     </script>
 @endsection
