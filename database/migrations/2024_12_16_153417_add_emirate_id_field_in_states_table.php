f42cce22119af9b68c686687ef3c9a8854d44f15
Atef Ben Ali <atef.bettaib@gmail.com>
2024-12-17 08:35:36 +0100

    feat(db): add `adding id field in states table` migration file

---
+++ database/migrations/2024_12_16_153417_add_emirate_id_field_in_states_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('states', function (Blueprint $table) {
+            $table->unsignedBigInteger('emirate_id');
+            $table->foreign('emirate_id')->references('id')->on('emirates');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('states', function (Blueprint $table) {
+           $table->removeColumn("emirate_id");
+        });
+    }
+};
