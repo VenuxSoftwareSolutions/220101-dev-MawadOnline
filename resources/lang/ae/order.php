4c61929987ca9196689f172829b470b50f311b99
Selmi Houssem <s.houssem@devnet.me>
2024-12-19 13:27:13 +0100

    feat: add `ae order` lang file

---
+++ resources/lang/ae/order.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+ 'in_preparation'         => 'في التحضير',
+ 'ready_for_shipment'     => 'جاهزة للشحن',
+ 'on_the_way'             => 'في الطريق',
+ 'total_quantity'         => 'الكمية الإجمالية',
+ 'quantity_entered_must_be_equal_to_the_quantity_requested'=>'يجب أن تكون الكمية المدخلة مساوية للكمية المطلوبة',
+];
