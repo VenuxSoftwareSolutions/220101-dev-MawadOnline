437fd87611a4a07c602b984c10ded282a8a15125
Selmi Houssem <s.houssem@devnet.me>
2024-12-25 11:54:57 +0100

    feat: add `confirm`  in lang file

--- resources/lang/en/order.php
+++ resources/lang/en/order.php
@@ -6,4 +6,5 @@ return [
  'on_the_way'             => 'On The Way',
  'total_quantity'         => 'Total Quantity',
  'quantity_entered_must_be_equal_to_the_quantity_requested'=>'The quantity entered must be equal to the quantity requested',
+ 'confirm_order' => 'Confirm order'
 ];
