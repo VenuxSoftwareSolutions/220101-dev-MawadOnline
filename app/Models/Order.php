532dbe044dbedc020c74e1bce9f860c00f7839c1
Atef Ben Ali <atef.bettaib@gmail.com>
2025-01-07 16:47:51 +0100

    feat(models|order): add `vendor` relationship func

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -55,4 +55,9 @@ class Order extends Model
     {
         return $this->hasMany(ProxyPayment::class)->select('reference_id');
     }
+
+    public function vendor()
+    {
+        return $this->belongsTo(User::class, 'seller_id', 'id');
+    }
 }
