1354ef86dfdcc63f8ef40b714f0db4e623a61401
Atef Ben Ali <atef.bettaib@gmail.com>
2025-01-07 16:47:07 +0100

    feat(models|ticket): add `getting vendor` func

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -6,8 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    public function user(){
-    	return $this->belongsTo(User::class);
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 
     public function ticketReplies()
@@ -15,8 +16,16 @@ class Ticket extends Model
         return $this->hasMany(TicketReply::class)->orderBy('created_at', 'desc');
     }
 
-    public function orderDetails(){
-    	return $this->belongsTo(OrderDetail::class);
+    public function orderDetails()
+    {
+        return $this->belongsTo(OrderDetail::class);
+    }
+
+    public function getVendor()
+    {
+        $sub_order_id = $this->orderDetails->order_id;
+
+        return Order::find($sub_order_id)->vendor;
     }
 
 }
