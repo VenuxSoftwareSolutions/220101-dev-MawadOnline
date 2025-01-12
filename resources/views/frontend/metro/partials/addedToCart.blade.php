4afb76662e14f4efcb278c5167e61e9cc5a9daa0
chouayeb landoulsi <chouayeb.landoulsi@gmail.com>
2025-01-12 13:38:29 +0100

    cart views

--- resources/views/frontend/metro/partials/addedToCart.blade.php
+++ resources/views/frontend/metro/partials/addedToCart.blade.php
@@ -1,36 +1,34 @@
-<div class="modal-body px-4 py-5 c-scrollbar-light">
+<div class="modal-body px-4 py-4 c-scrollbar-light">
     <!-- Item added to your cart -->
-    <div class="text-center text-success mb-4">
-        <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 36 36">
-            <g id="Group_23957" data-name="Group 23957" transform="translate(-6269 7766)">
-                <path id="Path_28713" data-name="Path 28713"
-                    d="M12.8,32.8a3.6,3.6,0,1,0,3.6,3.6A3.584,3.584,0,0,0,12.8,32.8ZM2,4V7.6H5.6l6.471,13.653-2.43,4.41A3.659,3.659,0,0,0,9.2,27.4,3.6,3.6,0,0,0,12.8,31H34.4V27.4H13.565a.446.446,0,0,1-.45-.45.428.428,0,0,1,.054-.216L14.78,23.8H28.19a3.612,3.612,0,0,0,3.15-1.854l6.435-11.682A1.74,1.74,0,0,0,38,9.4a1.8,1.8,0,0,0-1.8-1.8H9.587L7.877,4H2ZM30.8,32.8a3.6,3.6,0,1,0,3.6,3.6A3.584,3.584,0,0,0,30.8,32.8Z"
-                    transform="translate(6267 -7770)" fill="#85b567" />
-                <rect id="Rectangle_18068" data-name="Rectangle 18068" width="9" height="3" rx="1.5"
-                    transform="translate(6284.343 -7757.879) rotate(45)" fill="#fff" />
-                <rect id="Rectangle_18069" data-name="Rectangle 18069" width="3" height="13" rx="1.5"
-                    transform="translate(6295.657 -7760.707) rotate(45)" fill="#fff" />
-            </g>
-        </svg>
-        <h3 class="fs-28 fw-500">{{ translate('Item added to your cart!') }}</h3>
+    <div class="text-success mb-1 d-flex justify-content-start">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M9.61914 16L11.1191 17.5L14.3691 14.5" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M8.80945 2L5.18945 5.63" stroke="#292D32" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M15.1895 2L18.8095 5.63" stroke="#292D32" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M2 7.8501C2 6.0001 2.99 5.8501 4.22 5.8501H19.78C21.01 5.8501 22 6.0001 22 7.8501C22 10.0001 21.01 9.8501 19.78 9.8501H4.22C2.99 9.8501 2 10.0001 2 7.8501Z" stroke="#292D32" stroke-width="1.5"/>
+            <path d="M3.5 10L4.91 18.64C5.23 20.58 6 22 8.86 22H14.89C18 22 18.46 20.64 18.82 18.76L20.5 10" stroke="#292D32" stroke-width="1.5" stroke-linecap="round"/>
+            </svg>
+
+        <span class="fs-20 font-prompt-md ml-2 dark-c3">{{ translate('Item added to your cart!') }}</span>
     </div>
+    <hr>
 
     <!-- Product Info -->
     <div class="media mb-1">
         <img src="{{ static_asset('assets/img/placeholder.jpg') }}"
             data-src="{{ asset('/public/' . $product->getFirstImage()) }}"
-            class="mr-4 lazyload size-90px img-fit rounded-0" alt="Product Image"
+            class="mr-3 lazyload size-90px img-fit border-radius-8px" alt="Product Image"
             onerror="this.onerror=null;this.src='{{ static_asset('assets/img/placeholder.jpg') }}';">
         <div class="media-body mt-2 text-left d-flex flex-column justify-content-between">
-            <h6 class="fs-14 fw-700 text-truncate-2">
+            <h6 class="fs-16 font-prompt-md text-truncate-2">
                 {{ $product->getTranslation('name') }}
             </h6>
             <div class="row mt-2">
-                <div class="col-sm-3 fs-14 fw-400 text-secondary">
+                <div class="col-2 pr-0 fs-16 font-prompt text-secondary">
                     <div>{{ translate('Price') }}</div>
                 </div>
-                <div class="col-sm-9">
-                    <div class="fs-16 fw-700 text-primary">
+                <div class="col-9">
+                    <div class="fs-16 font-prompt-sb text-primary">
                         <strong>
                             {{ single_price((isset($samplePrice) ? $samplePrice : cart_product_price($cart, $product, false)) * $cart->quantity) }}
                         </strong>
