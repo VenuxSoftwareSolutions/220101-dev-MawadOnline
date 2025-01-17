9a07ff63a2de0a4ab656d6efc9adb9de243e0650
chouayeb landoulsi <chouayeb.landoulsi@gmail.com>
2025-01-17 14:13:21 +0100

    shipping page finish

--- resources/views/frontend/metro/partials/address_modal.blade.php
+++ resources/views/frontend/metro/partials/address_modal.blade.php
@@ -168,7 +168,7 @@
     <div class="modal-dialog modal-dialog-centered modal-md" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">{{ translate('New Address') }}</h5>
+                <h5 class="modal-title fs-17 font-prompt mt-1" id="exampleModalLabel">{{ translate('New Address') }}</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
