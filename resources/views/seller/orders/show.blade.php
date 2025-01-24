e9674b992cd89153021c8b0e4f512e86b74e05cc
Selmi Houssem <s.houssem@devnet.me>
2025-01-24 20:59:01 +0100

    feat(seller|orders show): disable order details `delivery status` when delivery status is `in-claim`

--- resources/views/seller/orders/show.blade.php
+++ resources/views/seller/orders/show.blade.php
@@ -167,7 +167,7 @@
                                     <td class="text-center">{{ single_price($orderDetail->price) }}</td>
                                     <td>
                                         <div class="row align-items-center justify-content-center">
-                                            @if ($orderDetail->delivery_status != 'delivered' && $orderDetail->delivery_status != 'cancelled')
+                                            @if ($orderDetail->delivery_status != 'delivered' && $orderDetail->delivery_status != 'cancelled' && $orderDetail->delivery_status != 'In-Claim')
                                                 @php
                                                     $shippers = explode(',', $orderDetail->product->shippingOptions($orderDetail->quantity)->shipper);
                                                 @endphp
@@ -221,7 +221,6 @@
                                     </td>
 
                                     <td>
-
                                        <select onchange="handleTicket(this)" class="form-control"
                                                 data-user_id="{{ $orderDetail->seller->id }}"
                                                 data-product_id="{{ $orderDetail->product->id }}"
@@ -239,7 +238,7 @@
                                                         {{$ticket->code}}-{{$ticket->subject}}
                                                     </option>
                                                 @endforeach
-                                            </select>
+                                        </select>
                                     </td>
                                 </tr>
                             @endforeach
@@ -620,7 +619,6 @@
         });
 
         const handleTicket = (event) =>{
-            console.log(event.value);
             if(event.value == "add_new_ticket"){
                 $('#ticket_modal').modal("show");
                 document.getElementById('order_details_id').value = event.dataset.orderdetail_id;
