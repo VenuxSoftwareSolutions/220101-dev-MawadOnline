6fb58872835f81cb15847c4b8ff3c5740f1947d1
Atef Ben Ali <atef.bettaib@gmail.com>
2025-01-27 12:02:42 +0100

    style: format

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -734,7 +734,6 @@ class CartController extends Controller
 
             // Calculate the price and tax
             $price = CartUtility::priceProduct($request->variationId, $request->quantity);
-
             $tax = 0;
 
             // Save the cart data
