c884b7dab9cce3b71e901d6db197603e1bfebacd
Selmi Houssem <s.houssem@devnet.me>
2025-01-30 11:46:13 +0100

    feat: add `replyTo` relationship to `TicketReply` Model

--- app/Models/TicketReply.php
+++ app/Models/TicketReply.php
@@ -12,4 +12,8 @@ class TicketReply extends Model
     public function user(){
     	return $this->belongsTo(User::class);
     }
+
+    public function replyTo(){
+    	return $this->belongsTo(User::class,'reply_to');
+    }
 }
