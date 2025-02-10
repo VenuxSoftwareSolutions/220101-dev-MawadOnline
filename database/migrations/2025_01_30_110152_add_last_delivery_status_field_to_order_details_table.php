508cbe3b8f2c2cbbaf73f79a896a49db2996de7d
Selmi Houssem <s.houssem@devnet.me>
2025-02-10 13:01:30 +0100

    refactor: add `last delivery status` field to `order details` table

---
+++ database/migrations/2025_01_30_110152_add_last_delivery_status_field_to_order_details_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('order_details', function (Blueprint $table) {
+            $table->enum('last_delivery_status',
+            ['pending','in_preparation','ready_for_shipment',
+            'on_the_way','delivered','cancelled','In-Claim'])
+            ->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('order_details', function (Blueprint $table) {
+           $table->dropColumn('last_delivery_status');
+        });
+    }
+};
