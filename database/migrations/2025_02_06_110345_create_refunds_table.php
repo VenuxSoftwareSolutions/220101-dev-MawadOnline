79fa04f4a84c1781b7f8a884d664f8c270bc9f16
Selmi Houssem <s.houssem@devnet.me>
2025-02-10 13:00:56 +0100

    feat: create `refunds` table

---
+++ database/migrations/2025_02_06_110345_create_refunds_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('refunds', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('buyer_id');
+            $table->unsignedBigInteger('seller_id');
+            $table->integer('order_detail_id');
+            $table->enum('refund_status',['error','success']);
+            $table->string('description_error', 255);
+            $table->string('payment_refund_id', 255);
+            $table->foreign('buyer_id')
+                    ->references('id')
+                    ->on('users');
+            $table->foreign('seller_id')
+                    ->references('id')
+                    ->on('users');
+            $table->foreign('order_detail_id')
+                    ->references('id')
+                    ->on('order_details');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('refunds');
+    }
+};
