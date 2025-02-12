e8c1c09a39486b7aa357037cf5a2b6d859be8ad6
Atef Ben Ali <atef.bettaib@gmail.com>
2025-02-12 12:38:06 +0100

    feat(db|seeder): add `inserting units table data` sql/seeder files

---
+++ database/data/units_inserts.sql
@@ -0,0 +1,75 @@
+INSERT INTO mawadonline.unites
+(id, name, created_at, updated_at, default_unit, rate)
+VALUES(3, '{"en":"Meters","ar":"\\u0645\\u062a\\u0631"}', '2023-12-13 08:04:14', '2023-12-13 08:04:49', 12, 0.01);
+INSERT INTO mawadonline.unites
+(id, name, created_at, updated_at, default_unit, rate)
+VALUES(5, '{"en":"Kilos","ar":"\\u0643\\u064a\\u0644\\u0648\\u063a\\u0631\\u0627\\u0645\\u0627\\u062a"}', '2023-12-14 06:18:10', '2023-12-14 06:18:10', NULL, 1.0);
+INSERT INTO mawadonline.unites
+(id, name, created_at, updated_at, default_unit, rate)
+VALUES(6, '{"en":"Kilometre","ar":"\\u0643\\u064a\\u0644\\u0648\\u0645\\u062a\\u0631"}', '2023-12-15 07:26:53', '2023-12-15 07:26:53', NULL, 1.0);
+INSERT INTO mawadonline.unites
+(id, name, created_at, updated_at, default_unit, rate)
+VALUES(7, '{"en":"mm","ar":"ملليمتر"}', '2024-05-09 16:29:38', '2024-05-09 16:29:38', 12, 10.0);
+INSERT INTO mawadonline.unites
+(id, name, created_at, updated_at, default_unit, rate)
+VALUES(9, '{"en":"decibels","ar":"ديسيبل"}', '2024-05-09 16:51:13', '2024-05-09 16:51:13', NULL, 1.0);
+INSERT INTO mawadonline.unites
+(id, name, created_at, updated_at, default_unit, rate)
+VALUES(10, '{"en":"Percent","ar":"في المائة"}', '2024-05-09 16:58:15', '2024-05-09 16:58:15', NULL, 1.0);
+INSERT INTO mawadonline.unites
+(id, name, created_at, updated_at, default_unit, rate)
+VALUES(11, '{"en":"minutes","ar":"دقيقة"}', '2024-05-09 17:03:13', '2024-05-09 17:03:13', NULL, 1.0);
+INSERT INTO mawadonline.unites
+(id, name, created_at, updated_at, default_unit, rate)
+VALUES(12, '{"en":"cm","ar":"سم"}', '2024-05-09 17:13:06', '2024-05-09 17:13:06', NULL, 1.0);
+INSERT INTO mawadonline.unites
+(id, name, created_at, updated_at, default_unit, rate)
+VALUES(13, '{"en":"gm","ar":"جم"}', '2024-05-09 17:20:37', '2024-05-09 17:20:37', 14, 0.001);
+INSERT INTO mawadonline.unites
+(id, name, created_at, updated_at, default_unit, rate)
+VALUES(14, '{"en":"kgm","ar":"كجم"}', '2024-05-09 17:21:01', '2024-05-09 17:21:01', NULL, 1.0);
+INSERT INTO mawadonline.unites
+(id, name, created_at, updated_at, default_unit, rate)
+VALUES(15, '{"en":"sqm","ar":"متر مربع"}', '2024-05-09 17:25:36', '2024-05-09 17:25:36', NULL, 1.0);
+INSERT INTO mawadonline.unites
+(id, name, created_at, updated_at, default_unit, rate)
+VALUES(16, '{"en":"KW","ar":"كيلوواط"}', '2024-05-09 17:35:51', '2024-05-09 17:35:51', 24, 1000.0);
+INSERT INTO mawadonline.unites
+(id, name, created_at, updated_at, default_unit, rate)
+VALUES(17, '{"en":"N/mn2","ar":"N/mn2"}', '2024-05-09 17:50:01', '2024-05-09 17:50:01', NULL, 1.0);
+INSERT INTO mawadonline.unites
+(id, name, created_at, updated_at, default_unit, rate)
+VALUES(18, '{"en":"bar","ar":"بار"}', '2024-05-09 17:56:40', '2024-05-09 17:56:40', NULL, 1.0);
+INSERT INTO mawadonline.unites
+(id, name, created_at, updated_at, default_unit, rate)
+VALUES(19, '{"en":"degree","ar":"درجة"}', '2024-05-09 18:08:47', '2024-05-09 18:08:47', NULL, 1.0);
+INSERT INTO mawadonline.unites
+(id, name, created_at, updated_at, default_unit, rate)
+VALUES(20, '{"en":"Volts","ar":"فولت"}', '2024-05-09 18:11:16', '2024-05-09 18:11:16', NULL, 1.0);
+INSERT INTO mawadonline.unites
+(id, name, created_at, updated_at, default_unit, rate)
+VALUES(21, '{"en":"ml","ar":"ملل"}', '2024-05-09 18:18:31', '2024-05-09 18:18:31', 22, 0.001);
+INSERT INTO mawadonline.unites
+(id, name, created_at, updated_at, default_unit, rate)
+VALUES(22, '{"en":"litre","ar":"لتر"}', '2024-05-09 18:18:48', '2024-05-09 18:18:48', NULL, 1.0);
+INSERT INTO mawadonline.unites
+(id, name, created_at, updated_at, default_unit, rate)
+VALUES(23, '{"en":"sec.","ar":"ثانية"}', '2024-05-09 18:26:36', '2024-05-09 18:26:36', NULL, 1.0);
+INSERT INTO mawadonline.unites
+(id, name, created_at, updated_at, default_unit, rate)
+VALUES(24, '{"en":"Watt","ar":"واط"}', '2024-05-09 18:31:35', '2024-05-09 18:31:35', NULL, 1.0);
+INSERT INTO mawadonline.unites
+(id, name, created_at, updated_at, default_unit, rate)
+VALUES(25, '{"en":"hours","ar":"ساعة"}', '2024-05-09 18:43:58', '2024-05-09 18:43:58', NULL, 1.0);
+INSERT INTO mawadonline.unites
+(id, name, created_at, updated_at, default_unit, rate)
+VALUES(26, '{"en":"ton","ar":"طن"}', '2024-05-09 18:53:01', '2024-05-09 18:53:01', 14, 1000.0);
+INSERT INTO mawadonline.unites
+(id, name, created_at, updated_at, default_unit, rate)
+VALUES(27, '{"en":"Newton","ar":"نيوتن"}', '2024-05-09 19:11:29', '2024-05-09 19:11:29', NULL, 1.0);
+INSERT INTO mawadonline.unites
+(id, name, created_at, updated_at, default_unit, rate)
+VALUES(28, '{"en":"kN","ar":"كيلو نيوتن"}', '2024-05-09 19:12:12', '2024-05-09 19:12:12', NULL, 1.0);
+INSERT INTO mawadonline.unites
+(id, name, created_at, updated_at, default_unit, rate)
+VALUES(29, '{"en":"N/mm2","ar":"N/mm2"}', '2024-05-09 19:24:29', '2024-05-09 19:24:29', NULL, 1.0);
