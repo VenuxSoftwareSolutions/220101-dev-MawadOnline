417df1d78dc65a518873b75d504786b9f96cf8e1
Selmi Houssem <s.houssem@devnet.me>
2025-02-12 14:39:47 +0100

    feat: add `payment_inten_id` to orders table

---
+++ database/migrations/2025_02_12_130726_add_payment_intent_id_to_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->string('payment_intent_id')->nullable()->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropIndex([
+                'payment_intent_id',
+            ]);
+
+            $table->dropColumn([
+                'payment_intent_id',
+            ]);
+        });
+    }
+};
