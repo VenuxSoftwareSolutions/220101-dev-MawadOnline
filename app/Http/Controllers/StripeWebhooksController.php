a1a5614f20b2c31fe8ae0eab17f0c793e5b9fcf7
Selmi Houssem <s.houssem@devnet.me>
2025-03-06 23:39:14 +0100

    refactor(StripWebHooks|Handle): review

--- app/Http/Controllers/StripeWebhooksController.php
+++ app/Http/Controllers/StripeWebhooksController.php
@@ -12,17 +12,25 @@ class StripeWebhooksController extends Controller
 {
     public function handle(Request $request){
         if($request->has("type") && $request->type == "charge.refund.updated"){
-            $refund_id = $request->data['object']['id'];
-            $charge_id = $request->data['object']['charge'];
-            $refund = Refund::where(['payment_refund_id' => $refund_id,'payment_charge_id'=>$charge_id])->first();
+            $result = $request->data['object'];
+            $refund_id = $result['id'];
+            $charge_id = $result['charge'];
+            $refund = Refund::where(['payment_refund_id' => $result['id'],'payment_charge_id' => $result['charge']])->first();
             if($refund){
                 Log::channel('webhooks')->info('webhook event received from refund changed',$request->all());
-                Log::channel('webhooks')->info('webhook event received details',array("type"=>$request->type,'refund_id'=>$request->data['object']['id'],'charge'=>$request->data['object']['charge'],'Model'=>$refund,'canceled'=>"yes"));
-                $refund->refund_status = $request->data['object']['status'];
+                Log::channel('webhooks')->info('webhook event received details',array("type"=>$request->type,'refund_id'=>$result['id'],'charge'=>$result['charge'],'Model'=>$refund,'canceled'=>"yes"));
+                $refund->refund_status = $result['status'];
+                $newRefund = new RefundHistories();
+                $newRefund->refund_status = $result['status'];
+                $newRefund->description_error = $result['failure_reason'];
+                $newRefund->payment_refund_id = $result['id'];
+                $newRefund->payment_charge_id = $result['charge'];
+                $newRefund->amount = number_format($result['amount']/100,2);
+                $newRefund->refund_id = $refund->id;
                 $refund->save();
+                $newRefund->save();
             }
         }
         Log::channel('webhooks')->info('webhook event received object  WEBHOOK',$request->all());
-        Log::channel('webhooks')->info('webhook event received type  WEBHOOK',array("type"=>$request->type,'refund_id'=>$request->data['object']['id'],'charge'=>$request->data['object']['charge']));
     }
 }
