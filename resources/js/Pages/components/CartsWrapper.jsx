457b8c57542327f28404b478ddc198f0cf181b93
Atef Ben Ali <atef.bettaib@gmail.com>
2025-03-12 13:15:22 +0100

    feat(resources|jsPagesComponent): add `carts wrapper` component

---
+++ resources/js/Pages/components/CartsWrapper.jsx
@@ -0,0 +1,49 @@
+import { usePage } from "@inertiajs/react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+
+import { Cart } from "./Cart";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "./slider.css";
+
+export function CartsWrapper() {
+    const {
+        props: { top10Categories },
+    } = usePage();
+
+    return (
+        <div className="container my-2">
+            <div className="row chart-slider-container">
+                <Swiper
+                    modules={[Navigation]}
+                    spaceBetween={2}
+                    slidesPerView={3.5}
+                    navigation={{
+                        nextEl: ".custom-next",
+                        prevEl: ".custom-prev",
+                    }}
+                >
+                    {top10Categories.map(
+                        ({ category, parentCategory, evolution }, index) => (
+                            <SwiperSlide
+                                key={`${category}_${parentCategory}-${index}`}
+                            >
+                                <div className="col">
+                                    <Cart
+                                        category={category}
+                                        subCategory={parentCategory}
+                                        evolution={evolution}
+                                    />
+                                </div>
+                            </SwiperSlide>
+                        )
+                    )}
+                </Swiper>
+                <button className="custom-prev" />
+                <button className="custom-next" />
+            </div>
+        </div>
+    );
+}
