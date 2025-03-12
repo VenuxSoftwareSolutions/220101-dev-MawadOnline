dd42204a92b1798517b17d40a4a9bd83cc1e9751
Atef Ben Ali <atef.bettaib@gmail.com>
2025-03-12 13:14:39 +0100

    refactor(resources|jsPagesHome): review `logic`

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,14 +1,8 @@
-import { usePage } from "@inertiajs/react";
-
-import Cart from "./components/Cart";
-import HeaderContainer from "./components/HeaderContainer";
-import MaterialsTrendsContainer from "./components/MaterialsTrendsContainer";
+import { HeaderContainer } from "./components/HeaderContainer";
+import { MaterialsTrendsContainer } from "./components/MaterialsTrendsContainer";
+import { CartsWrapper } from "./components/CartsWrapper";
 
 export default function Home() {
-    const {
-        props: { categories, top10Categories },
-    } = usePage();
-
     return (
         <div
             className="px-5 pt-5 pb-2"
@@ -16,33 +10,13 @@ export default function Home() {
         >
             <section className="px-5 pt-5 pb-2">
                 <HeaderContainer />
-                <div className="container my-2">
-                    <div className="row">
-                        {top10Categories.map(
-                            (
-                                { category, parentCategory, evolution },
-                                index
-                            ) => (
-                                <div
-                                    key={`${category}_${parentCategory}-${index}`}
-                                    className="col"
-                                >
-                                    <Cart
-                                        title={category}
-                                        subTitle={parentCategory}
-                                        evolution={evolution}
-                                    />
-                                </div>
-                            )
-                        )}
-                    </div>
-                </div>
+                <CartsWrapper />
                 <div className="container">
                     <div className="row">
                         <div className="col">Coming soon</div>
                     </div>
                 </div>
-                <MaterialsTrendsContainer categories={categories} />
+                <MaterialsTrendsContainer />
             </section>
         </div>
     );
