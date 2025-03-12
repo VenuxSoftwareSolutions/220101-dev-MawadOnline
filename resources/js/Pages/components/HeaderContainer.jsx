1f82a49c6c6dab636bc1a13580d59cb9f63c244b
Atef Ben Ali <atef.bettaib@gmail.com>
2025-03-12 13:10:50 +0100

    feat(resources|jsPagesComponentHeaderContainer): add `btns filters` logic

--- resources/js/Pages/components/HeaderContainer.jsx
+++ resources/js/Pages/components/HeaderContainer.jsx
@@ -1,6 +1,19 @@
-export default function HeaderContainer() {
+import { usePage, router } from "@inertiajs/react";
+import { route } from "ziggy-js";
+
+export function HeaderContainer() {
     const textPlaceholder =
         "Quod at accusamus nostrum quidem numquam. Atque iusto quos vel rerum nemo. Sequi animi ad odio.";
+
+    const { filter } = usePage().props;
+
+    const handleFilterChange = (newFilter) => {
+        router.get(
+            route("mawad.index"),
+            { filter: newFilter },
+            { preserveScroll: true, preserveState: true }
+        );
+    };
 
     return (
         <div className="container my-2">
@@ -10,10 +23,24 @@ export default function HeaderContainer() {
                     <p>{textPlaceholder}</p>
                 </div>
                 <div className="col-4 d-flex justify-content-end">
-                    <button className="my-3 btn btn-primary btn-sm mr-2">
+                    <button
+                        className={`my-3 btn btn-primary btn-sm mr-2 ${
+                            filter === "avg"
+                                ? "bg-orange-500 text-white"
+                                : "bg-gray-300"
+                        }`}
+                        onClick={() => handleFilterChange("avg")}
+                    >
                         Avg. Price
                     </button>
-                    <button className="my-3 btn btn-primary btn-sm">
+                    <button
+                        className={`my-3 btn btn-primary btn-sm ${
+                            filter === "lowest"
+                                ? "bg-gray-500 text-white"
+                                : "bg-gray-300"
+                        }`}
+                        onClick={() => handleFilterChange("lowest")}
+                    >
                         Lowest Price
                     </button>
                 </div>
