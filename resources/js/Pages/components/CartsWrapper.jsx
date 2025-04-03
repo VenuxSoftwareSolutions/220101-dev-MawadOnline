20127471e0b75b411c191ffb81458d581c44434b
Atef Ben Ali <atef.bettaib@gmail.com>
2025-04-03 09:42:15 +0100

    fix(js|pagesComponentsCartsWrapper): handle `no data` case

--- resources/js/Pages/components/CartsWrapper.jsx
+++ resources/js/Pages/components/CartsWrapper.jsx
@@ -1,8 +1,10 @@
+import { Fragment } from "react";
 import { usePage } from "@inertiajs/react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 
 import { Cart } from "./Cart";
+import { NoDataCart } from "./NoDataCart";
 
 import "swiper/css";
 import "swiper/css/navigation";
@@ -10,51 +12,62 @@ import "swiper/css/navigation";
 import "./slider.css";
 
 export function CartsWrapper() {
-    const {
-        props: { top10CategoriesEvolution },
-    } = usePage();
+    const { top10CategoriesEvolution } = usePage().props;
 
-    const slidesPerView = 2.5;
+    const slidesPerView =
+        top10CategoriesEvolution.length > 2
+            ? top10CategoriesEvolution.length / 2
+            : 1.5;
 
     return (
         <div className="container my-2">
             <div className="row chart-slider-container">
-                <Swiper
-                    modules={[Navigation]}
-                    spaceBetween={2}
-                    slidesPerView={slidesPerView}
-                    navigation={{
-                        nextEl: ".custom-next",
-                        prevEl: ".custom-prev",
-                    }}
-                >
-                    {top10CategoriesEvolution.map(
-                        (
-                            {
-                                category,
-                                parentCategory,
-                                evolution,
-                                priceChange,
-                            },
-                            index
-                        ) => (
-                            <SwiperSlide
-                                key={`${category}_${parentCategory}-${index}`}
-                            >
-                                <div className="col">
-                                    <Cart
-                                        category={category}
-                                        subCategory={parentCategory}
-                                        evolution={evolution}
-                                        priceChange={priceChange}
-                                    />
-                                </div>
-                            </SwiperSlide>
-                        )
-                    )}
-                </Swiper>
-                <button className="custom-prev" />
-                <button className="custom-next" />
+                {top10CategoriesEvolution.length === 0 ? (
+                    <div className="col">
+                        <NoDataCart />
+                    </div>
+                ) : (
+                    <Swiper
+                        modules={[Navigation]}
+                        spaceBetween={2}
+                        slidesPerView={slidesPerView}
+                        navigation={{
+                            nextEl: ".custom-next",
+                            prevEl: ".custom-prev",
+                        }}
+                    >
+                        {top10CategoriesEvolution.map(
+                            (
+                                {
+                                    category,
+                                    parentCategory,
+                                    evolution,
+                                    priceChange,
+                                },
+                                index
+                            ) => (
+                                <SwiperSlide
+                                    key={`${category}_${parentCategory}-${index}`}
+                                >
+                                    <div className="col">
+                                        <Cart
+                                            category={category}
+                                            subCategory={parentCategory}
+                                            evolution={evolution}
+                                            priceChange={priceChange}
+                                        />
+                                    </div>
+                                </SwiperSlide>
+                            )
+                        )}
+                    </Swiper>
+                )}
+                {top10CategoriesEvolution.length > 0 ? (
+                    <Fragment>
+                        <button className="custom-prev" />
+                        <button className="custom-next" />
+                    </Fragment>
+                ) : null}
             </div>
         </div>
     );
---
+++ resources/js/Pages/components/NoDataCart.jsx
@@ -0,0 +1,11 @@
+export function NoDataCart() {
+    return (
+        <div className="card">
+            <div className="card-body">
+                <div className="p-4 d-flex justify-content-center align-items-center">
+                    <h5>No Data Available</h5>
+                </div>
+            </div>
+        </div>
+    );
+}
