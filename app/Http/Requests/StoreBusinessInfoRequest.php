e520b58c1b7b1e572d3c92460b56f975575e9315
alaeddineth <theljanialaeddine@gmail.com>
2025-04-17 11:22:04 +0100

    feat(antivirus-scan):add clamav scan for tax_waiver document

--- app/Http/Requests/StoreBusinessInfoRequest.php
+++ app/Http/Requests/StoreBusinessInfoRequest.php
@@ -62,7 +62,7 @@ class StoreBusinessInfoRequest extends FormRequest
             'trade_license_doc' => 'nullable|file|max:5120|mimes:pdf,jpg,jpeg,png,webp,gif,avif,bmp,tiff,heic|clamav',
             'trn' => $this->vat_registered == 1 ? 'nullable|string|max:20' : '',
             'vat_certificate' => $this->vat_registered == 1 /* && !isset($this->vat_certificate_old) */ ? 'nullable|file|mimes:pdf,jpeg,png|max:5120|clamav' : '',
-            'tax_waiver' => 'nullable|file|max:5120|mimes:pdf,jpg,jpeg,png,webp,gif,avif,bmp,tiff,heic',
+            'tax_waiver' => 'nullable|file|max:5120|mimes:pdf,jpg,jpeg,png,webp,gif,avif,bmp,tiff,heic|clamav',
             'civil_defense_approval' => 'nullable|file|max:5120|mimes:pdf,jpg,jpeg,png,webp,gif,avif,bmp,tiff,heic|clamav',
 
         ];
