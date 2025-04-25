6c0cf8bb94d6a70d01a0f79c8f401f2789df5c23
Selmi Houssem <s.houssem@devnet.me>
2025-04-26 00:02:22 +0200

    feat: add `fee details` to `refund & refund histories` tables

---
+++ database/migrations/2025_04_25_184134_add_fields_payment_balance_id_and_fee_amout_to_refunds_histories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('refund_histories', function (Blueprint $table) {
+            $table->double('fee_amount')->nullable();
+            $table->json('fee_details')->nullable();
+            $table->string('payment_balance_id', 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('refunds_histories', function (Blueprint $table) {
+            //
+        });
+    }
+};
