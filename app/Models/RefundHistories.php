77d61a00514402cc8866539c50dcfd6457744d1f
Selmi Houssem <s.houssem@devnet.me>
2025-04-25 23:59:02 +0200

    feat: add `fee details` to casts attribute in `Refund Histories` Model

--- app/Models/RefundHistories.php
+++ app/Models/RefundHistories.php
@@ -8,7 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class RefundHistories extends Model
 {
     use HasFactory;
-
+    protected $casts = [
+        'fee_details' => 'array'
+    ];
     public function refund()
     {
         return $this->belongsTo(Refund::class);
