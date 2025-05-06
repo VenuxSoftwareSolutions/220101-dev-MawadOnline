cf74d146feb03f7c07134c40238ebbe9ecab314a
Atef Ben Ali <atef.bettaib@gmail.com>
2025-05-06 10:09:23 +0100

    feat(db|migration): add `adding discount share in order details table` migration file

---
+++ database/migrations/2025_05_06_073810_add_discount_share_in_order_details_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('order_details', function (Blueprint $table) {
+            $table->double('discount_share', 20, 2)->default(0)->after('tax');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('order_details', function (Blueprint $table) {
+            $table->dropColumn('discount_share');
+        });
+    }
+};
